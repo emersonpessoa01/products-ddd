0426ec9db64e8d5051d3ed6016ff93546d6c4391
emersonpessoa <emersonpessoa05102008@gmail.com>
2025-06-17 15:08:15 -0300

    feat: Created Product on Service and Added ProductService to the Product :rocket:

--- src/main/java/br/com/raroacademy/products_ddd/service/produto/ProductService.java
+++ src/main/java/br/com/raroacademy/products_ddd/service/produto/ProductService.java
@@ -1,7 +1,7 @@
 package br.com.raroacademy.products_ddd.service;
 
-import br.com.raroacademy.products_ddd.domain.dto.ProductDTO;
-import br.com.raroacademy.products_ddd.domain.entity.Product;
+import br.com.raroacademy.products_ddd.domain.produto.dto.ProductDTO;
+import br.com.raroacademy.products_ddd.domain.produto.entity.Product;
 import br.com.raroacademy.products_ddd.infraestructure.db.jpa.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
